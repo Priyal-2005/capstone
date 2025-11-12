25592be78d35231514d6d2856bff856a59676dc3
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-11-12 19:06:03 +0530

    added changes

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,7 @@ app.post("/register", async (req, res) => {
 
 // login
 app.post("/login", async (req, res) => {
+    console.log(req.body)
     try {
         const {email, password} = req.body;
         const user = await prisma.user.findUnique({
