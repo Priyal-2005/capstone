4f1b76c3de401b6725590f625770b4550be04599
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-11-12 19:04:02 +0530

    added changes

---
+++ frontend/help.js
@@ -0,0 +1,20 @@
+fetch("https://capstone-nine-black.vercel.app/login", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-GB,en;q=0.9",
+      "content-type": "application/json",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Chromium\";v=\"142\", \"Brave\";v=\"142\", \"Not_A Brand\";v=\"99\"",
+      "sec-ch-ua-mobile": "?1",
+      "sec-ch-ua-platform": "\"Android\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sec-gpc": "1"
+    },
+    "referrer": "https://capstone-nine-black.vercel.app/",
+    "body": "{\"email\":\"priyal@email.com\",\"password\":\"1234\"}",
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "omit"
+  });
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -12,7 +12,7 @@ function Login({onLogin, goToRegister}) {
             return;
         }
         try {
-            const response = await fetch("login", {
+            const response = await fetch("/login", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({email, password})
