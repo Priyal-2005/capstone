ca2f959a7b853f71bb537d186f1fe2e3ad3dacc1
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-12-01 19:04:01 +0530

    added pagination and sorting

--- backend/src/controllers/doctor.controller.js
+++ backend/src/controllers/doctor.controller.js
@@ -4,8 +4,58 @@ const prisma = require("../config/prisma");
 
 const getDoctorList = async (req, res) => {
     try {
-        const doctors = await prisma.doctor.findMany();
-        return res.status(200).json({data: doctors});
+        let {page = 1, limit = 10, search = "", specialization = "", sort = "name", order = "asc"} = req.query;
+
+        page = parseInt(page);
+        limit = parseInt(limit);
+
+        // Order
+        const sortField = sort === "name" ? "user" : sort;
+        const sortSettings =
+            sort === "name"
+            ? { user: { name: order } }
+            : { [sort]: order };
+        
+        // Filters
+        const where = {
+            AND: [
+              specialization ? { specialization: { contains: specialization, mode: "insensitive" } } : {},
+              search
+                ? {
+                    OR: [
+                      { specialization: { contains: search, mode: "insensitive" } },
+                      { user: { name: { contains: search, mode: "insensitive" } } },
+                      { user: { email: { contains: search, mode: "insensitive" } } }
+                    ]
+                  }
+                : {}
+            ]
+        }
+
+        const doctors = await prisma.doctor.findMany({
+            where,
+            skip: (page - 1) * limit,
+            take: limit,
+            orderBy: sortSettings,
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true
+                    }
+                }
+            }
+        })
+
+        const total = await prisma.doctor.count({ where })
+
+        return res.status(200).json({
+            page,
+            limit,
+            total,
+            data: doctors
+        })
     }
     catch (error) {
         console.log(error)
@@ -15,10 +65,17 @@ const getDoctorList = async (req, res) => {
 
 const getDoctorById = async (req, res) => {
     try {
-        const id = parseInt(req.params.id);
+        const id = parseInt(req.params.id)
         const doctor = await prisma.doctor.findUnique({
-            where: {doctorId: id}
+        where: { id },
+        include: {
+            user: {
+            select: { id: true, name: true, email: true }
+            }}
         })
+        if (!doctor) {
+        return res.status(404).json({message: "Doctor not found"});
+        }
 
         return res.status(200).json({data: doctor});
     }
