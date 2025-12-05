0c990a9977d88c6fb1e6ab83b6dd50357d83a6bc
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-12-05 19:14:55 +0530

    added change

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
 app.use(cookieParser());
 
 // Routes
