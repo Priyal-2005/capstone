bd869042a79fe68b1888091270c37e4e066eea94
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-12-05 18:37:40 +0530

    url chnaged

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 // Allow cookies from frontend
 app.use(cors({
-  origin: ["http://localhost:5173", "https://capstone-seven-liart.vercel.app"],
+  origin: ["http://localhost:5173", "https://capstone-seven-liart.vercel.app", "https://capstone-git-main-priyal-2005s-projects.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
